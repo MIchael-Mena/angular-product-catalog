6f01fe043a65b96eac901250416857ef90bf08c4
Michael Mena <michael.mena2408@gmail.com>
2023-06-14 13:47:08 -0300

    update angular 16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IconsComponent } from './components/icons/icons.component';
-import {MaterialModule} from "./shared/material.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    IconsComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MaterialModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
