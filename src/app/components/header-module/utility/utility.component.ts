8c7d1c3f6f2b6ecc30ff3856e1d1fca1bee0be2d
Michael Mena <michael.mena2408@gmail.com>
2023-06-20 00:29:39 -0300

    login base

--- src/app/components/header-module/utility/utility.component.ts
+++ src/app/components/header-module/utility/utility.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {ShoppingCartService} from "../../../service/shopping-cart.service";
 import {MatDialog} from "@angular/material/dialog";
 import {ConfirmationDialogComponent} from "../../confirmation-dialog/confirmation-dialog.component";
+import {LoginComponent} from "../../login-register/login/login.component";
+import {RegisterComponent} from "../../login-register/register/register.component";
 
 @Component({
   selector: 'app-utility',
@@ -43,6 +45,30 @@ export class UtilityComponent {
       if (result) {
         this.cleanCart();
       }
+    });
+  }
+
+  public openLoginModal(): void {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '300px',
+      disableClose: true,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'register') {
+        this.openRegisterDialog();
+      }
+    });
+  }
+
+  openRegisterDialog(): void {
+    const dialogRef = this.dialog.open(RegisterComponent, {
+      width: '300px',
+      disableClose: true,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // Aquí puedes realizar acciones adicionales después de que se cierre el diálogo de registro
     });
   }
 
